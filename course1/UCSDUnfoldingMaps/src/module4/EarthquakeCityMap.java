9bc00c1a80394b15a800b1a447380cd1b158a020
Jixin Feng <ufjfeng@users.noreply.github.com>
2017-03-28 11:54:23 -0400

    modify data feed url

--- course1/UCSDUnfoldingMaps/src/module4/EarthquakeCityMap.java
+++ course1/UCSDUnfoldingMaps/src/module4/EarthquakeCityMap.java
@@ -35,13 +35,13 @@ public class EarthquakeCityMap extends PApplet {
 	private static final long serialVersionUID = 1L;
 
 	// IF YOU ARE WORKING OFFILINE, change the value of this variable to true
-	private static final boolean offline = true;
+	private static final boolean offline = false;
 	
 	/** This is where to find the local tiles, for working without an Internet connection */
 	public static String mbTilesString = "blankLight-1-3.mbtiles";
 
 	//feed with magnitude 2.5+ Earthquakes
-	private String earthquakesURL = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
+	private String earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
 	
 	// The files containing city names and info and country names and info
 	private String cityFile = "city-data.json";

db643878a747df91a0e47d3d3970faeca270a728
Jixin Feng <ufjfeng@users.noreply.github.com>
2017-05-16 16:17:13 -0400

    add MapEdge and MapNode class

--- course3/UCSDGraphs/src/roadgraph/MapEdge.java
+++ course3/UCSDGraphs/src/roadgraph/MapEdge.java
@@ -1,5 +1,66 @@
 package roadgraph;
 
-class MapEdge {
+import java.util.HashSet;
+import geography.GeographicPoint;
 
+class MapEdge {
+	private GeographicPoint fromLoc;
+	private GeographicPoint toLoc;
+	
+	private String roadName;
+	private String roadType;
+	private double length;
+	
+	public MapEdge(
+			GeographicPoint fromLoc, 
+			GeographicPoint toLoc, 
+			String roadName, 
+			String roadType, 
+			double length) {
+		this.fromLoc = fromLoc;
+		this.toLoc = toLoc;
+		this.roadName = roadName;
+		this.roadType = roadType;
+		this.length = length;
+	}
+
+	public GeographicPoint getFromLoc() {
+		return fromLoc;
+	}
+
+	public void setFromLoc(GeographicPoint fromLoc) {
+		this.fromLoc = fromLoc;
+	}
+
+	public GeographicPoint getToLoc() {
+		return toLoc;
+	}
+
+	public void setToLoc(GeographicPoint toLoc) {
+		this.toLoc = toLoc;
+	}
+
+	public String getRoadName() {
+		return roadName;
+	}
+
+	public void setRoadName(String roadName) {
+		this.roadName = roadName;
+	}
+
+	public String getRoadType() {
+		return roadType;
+	}
+
+	public void setRoadType(String roadType) {
+		this.roadType = roadType;
+	}
+
+	public double getLength() {
+		return length;
+	}
+
+	public void setLength(double length) {
+		this.length = length;
+	}
 }
--- course3/UCSDGraphs/src/roadgraph/MapNode.java
+++ course3/UCSDGraphs/src/roadgraph/MapNode.java
@@ -1,5 +1,35 @@
 package roadgraph;
 
-class MapNode {
+import java.util.HashSet;
+import geography.GeographicPoint;
 
+class MapNode {
+	private GeographicPoint location;
+	private HashSet<MapEdge> outEdges;
+	
+	public MapNode(GeographicPoint location) {
+		this.location = location;
+		outEdges = new HashSet<MapEdge>();
+	}
+	
+	public boolean addOutEdge(MapEdge edge) {
+		if (edge.getFromLoc().equals(this.location)) {
+			outEdges.add(edge);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public GeographicPoint getLocation() {
+		return location;
+	}
+
+	public void setLocation(GeographicPoint location) {
+		this.location = location;
+	}
+
+	public HashSet<MapEdge> getOutEdges() {
+		return outEdges;
+	}
 }
